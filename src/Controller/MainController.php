5a850be28b4d2670d950314bdf52bad2950ba4ee
philippe <philippe.frager@wanadoo.fr>
2024-07-24 18:23:33 +0200

    Instal Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Subaqua Biologie</strong>');
+        return $this->render('main/homepage.html.twig');
     }
 }
